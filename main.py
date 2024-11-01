abbd99bdb7974e4ed8d8047cc5e0e3d627838a83
Pronoy K Isaac <pronoykisaac@gmail.com>
2024-11-02 02:01:28 +0530

    chore: files

--- main.py
+++ main.py
@@ -48,4 +48,4 @@ def send_mail(sender_email, receiver_email, subject, body, password):
     return 1
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
